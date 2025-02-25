b487736a6dda59f0bedaf1bed6943ca85834c802
Dimitri Imfeld <dimfeld@protonmail.ch>
2025-02-25 08:46:09 +0100

    Add docstring for color_zones

--- color_zone.py
+++ color_zone.py
@@ -1,35 +1,35 @@
-from PIL import Image
+from PIL import Image 
 
 type Color = tuple[int,int,int]
 
 def color_zones(base: Image, mask: Image, replacements: dict[Color, Color]) -> Image:
-    # create an image to store the new texture
+    """
+    Replaces colors in parts the base image according to 
+    zones in the mask image and colors in the replacements dictionary.
+
+    Args:
+        base: The base image to colorize.
+        mask: The mask image that defines the zones to colorize.
+        replacements: A dictionary that maps mask colors to a replacement color.
+    """
+
+    if base.size != mask.size:
+        raise ValueError("Base and mask images must have the same size")
+
     out: Image = Image.new("RGB", base.size)
+
+    # Make sure images are in RGB mode
+    base = base.convert("RGB")
+    mask = mask.convert("RGB")
 
     for y in range(base.size[1]):
         for x in range(base.size[0]):
             mask_color = mask.getpixel((x, y))
             base_color = base.getpixel((x, y))
 
-            # replace the color if it is in the replacements dictionary, otherwise keep the original color
+            # Get the replacement color or use the base color
             out_color = replacements.get(mask_color, base_color)
 
             out.putpixel((x, y), out_color)
 
     return out
-
-
-if __name__ == "__main__":
-    base_file = "duck_base.png"
-    mask_file = "duck_mask.png"
-    replacements = {
-        (255, 0, 0): (0, 255, 200),
-        (0, 255, 0): (0, 255, 255),
-    }
-
-    # read base and mask files
-    base_image = Image.open(base_file).convert("RGB")
-    mask_image = Image.open(mask_file).convert("RGB")
-
-    out = color_zones(base_image, mask_image, replacements)
-    out.save("duck_colored.png")
